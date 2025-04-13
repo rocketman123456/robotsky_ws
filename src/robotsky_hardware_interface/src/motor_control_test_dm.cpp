1c3ed3677643428807ee4ca3041f9c7b237ab06c
rocketman123456 <759094438@qq.com>
2025-04-13 23:54:55 +0800

    [ADD] add multi-motor-test

--- src/robotsky_hardware_interface/src/motor_control_test_dm.cpp
+++ src/robotsky_hardware_interface/src/motor_control_test_dm.cpp
@@ -28,33 +28,45 @@ int main(int argc, char** argv)
 
     can_frame can_tx;
     can_frame can_rx;
-    uint16_t  can_id    = 0x0c;
-    uint16_t  can_index = 1;
+
+    // uint16_t  can_id    = 0x0c;
+    // uint16_t  can_index = 1;
+
+    uint16_t can_ids[] = {0x01, 0x04, 0x05, 0x08, 0x09, 0x0c, 0x0d, 0x10};
+    uint16_t can_indexs[] = {0, 0, 0, 0, 1, 1, 1, 1};
+
+    uint16_t motor_count = 8;
 
     dm_motor_fb_t data;
 
-    dm_enable_motor_mode(can_tx, can_id, DM_MIT_MODE);
+    for(int i = 0; i < motor_count; ++i)
     {
-        driver.send(can_index, can_tx);
-        usleep(50);
-        driver.receive(can_index, can_rx);
+        dm_enable_motor_mode(can_tx, can_ids[i], DM_MIT_MODE);
+        {
+            driver.send(can_indexs[i], can_tx);
+            usleep(50);
+            driver.receive(can_indexs[i], can_rx);
+        }
+        dm_decode(can_rx, data);
     }
-    dm_decode(can_rx, data);
 
     rclcpp::Rate loop_rate(100);
     try
     {
         while (rclcpp::ok())
         {
-            dm_mit_ctrl(can_tx, can_id, 0.0f, 0.0f, 0.0f, 2.0f, 0.0f);
+            for(int i = 0; i < motor_count; ++i)
             {
-                driver.send(can_index, can_tx);
-                usleep(50);
-                driver.receive(can_index, can_rx);
+                dm_mit_ctrl(can_tx, can_ids[i], 0.0f, 0.0f, 0.0f, 2.0f, 0.0f);
+                {
+                    driver.send(can_indexs[i], can_tx);
+                    usleep(50);
+                    driver.receive(can_indexs[i], can_rx);
+                }
+                dm_decode(can_rx, data);
             }
-            dm_decode(can_rx, data);
 
-            spdlog::info("motor {} pos : {}", data.id, data.pos);
+            // spdlog::info("motor {} pos : {}", data.id, data.pos);
 
             loop_rate.sleep();
         }
@@ -64,13 +76,16 @@ int main(int argc, char** argv)
         spdlog::warn("runtime error!");
     }
 
-    dm_disable_motor_mode(can_tx, can_id, DM_MIT_MODE);
+    for(int i = 0; i < motor_count; ++i)
     {
-        driver.send(can_index, can_tx);
-        usleep(50);
-        driver.receive(can_index, can_rx);
+        dm_disable_motor_mode(can_tx, can_ids[i], DM_MIT_MODE);
+        {
+            driver.send(can_indexs[i], can_tx);
+            usleep(50);
+            driver.receive(can_indexs[i], can_rx);
+        }
+        dm_decode(can_rx, data);
     }
-    dm_decode(can_rx, data);
 
     rclcpp::shutdown();
     return 0;
--- src/robotsky_hardware_interface/src/motor_control_test_rs.cpp
+++ src/robotsky_hardware_interface/src/motor_control_test_rs.cpp
@@ -28,43 +28,56 @@ int main(int argc, char** argv)
 
     can_frame can_tx;
     can_frame can_rx;
-    uint16_t  can_id    = 0x0a;
-    uint16_t  can_index = 1;
+
+    // uint16_t  can_id    = 0x0a;
+    // uint16_t  can_index = 1;
+
+    uint16_t can_ids[] = {0x02, 0x03, 0x06, 0x07, 0x0a, 0x0b, 0x0e, 0x0f};
+    uint16_t can_indexs[] = {0, 0, 0, 0, 1, 1, 1, 1};
+
+    uint16_t motor_count = 8;
 
     rs_motor_fb_t      data;
     rs_data_read_write data_motor;
 
-    rs_enable_motor_mode(can_tx, can_id);
+    for(int i = 0; i < motor_count; ++i)
     {
-        driver.send(can_index, can_tx);
-        usleep(50);
-        driver.receive(can_index, can_rx);
+        rs_enable_motor_mode(can_tx, can_ids[i]);
+        {
+            driver.send(can_indexs[i], can_tx);
+            usleep(50);
+            driver.receive(can_indexs[i], can_rx);
+        }
+        rs_decode(can_rx, data, data_motor);
     }
-    rs_decode(can_rx, data, data_motor);
 
-    rs_set_motor_parameter(can_tx, can_id, 0X7005, RS_Move_Control_mode, RS_Set_mode);
+    for(int i = 0; i < motor_count; ++i)
     {
-        driver.send(can_index, can_tx);
-        usleep(50);
-        driver.receive(can_index, can_rx);
-    }
-    rs_decode(can_rx, data, data_motor);
+        rs_set_motor_parameter(can_tx, can_ids[i], 0X7005, RS_Move_Control_mode, RS_Set_mode);
+        {
+            driver.send(can_indexs[i], can_tx);
+            usleep(50);
+            driver.receive(can_indexs[i], can_rx);
+        }
+        rs_decode(can_rx, data, data_motor);
+    
 
-    rs_get_motor_parameter(can_tx, can_id, 0X7005);
-    {
-        driver.send(can_index, can_tx);
-        usleep(50);
-        driver.receive(can_index, can_rx);
+        // rs_get_motor_parameter(can_tx, can_ids[i], 0X7005);
+        // {
+        //     driver.send(can_indexs[i], can_tx);
+        //     usleep(50);
+        //     driver.receive(can_indexs[i], can_rx);
+        // }
+        // rs_decode(can_rx, data, data_motor);
     }
-    rs_decode(can_rx, data, data_motor);
 
-    rs_mit_ctrl(can_tx, can_id, 0.0f, 0.0f, 0.0f, 2.0f, 0.0f);
-    {
-        driver.send(can_index, can_tx);
-        usleep(50);
-        driver.receive(can_index, can_rx);
-    }
-    rs_decode(can_rx, data, data_motor);
+    // rs_mit_ctrl(can_tx, can_ids[i], 0.0f, 0.0f, 0.0f, 2.0f, 0.0f);
+    // {
+    //     driver.send(can_indexs[i], can_tx);
+    //     usleep(50);
+    //     driver.receive(can_indexs[i], can_rx);
+    // }
+    // rs_decode(can_rx, data, data_motor);
 
     spdlog::info("start");
 
@@ -86,16 +99,19 @@ int main(int argc, char** argv)
             //     usleep(50);
             //     driver.receive(can_index, can_rx);
             // }
-
-            rs_mit_ctrl(can_tx, can_id, 0.0f, 0.0f, 0.0f, 2.0f, 0.0f);
+            
+            for(int i = 0; i < motor_count; ++i)
             {
-                driver.send(can_index, can_tx);
-                usleep(50);
-                driver.receive(can_index, can_rx);
+                rs_mit_ctrl(can_tx, can_ids[i], 0.0f, 0.0f, 0.0f, 2.0f, 0.0f);
+                {
+                    driver.send(can_indexs[i], can_tx);
+                    usleep(50);
+                    driver.receive(can_indexs[i], can_rx);
+                }
+                rs_decode(can_rx, data, data_motor);
             }
-            rs_decode(can_rx, data, data_motor);
 
-            spdlog::info("motor {} pos : {}", data.id, data.pos);
+            // spdlog::info("motor {} pos : {}", data.id, data.pos);
 
             loop_rate.sleep();
         }
@@ -105,13 +121,16 @@ int main(int argc, char** argv)
         spdlog::warn("runtime error!");
     }
 
-    rs_disable_motor_mode(can_tx, can_id, 0);
+    for(int i = 0; i < motor_count; ++i)
     {
-        driver.send(can_index, can_tx);
-        usleep(50);
-        driver.receive(can_index, can_rx);
+        rs_disable_motor_mode(can_tx, can_ids[i], 0);
+        {
+            driver.send(can_indexs[i], can_tx);
+            usleep(50);
+            driver.receive(can_indexs[i], can_rx);
+        }
+        rs_decode(can_rx, data, data_motor);
     }
-    // dm_decode(can_rx, data);
 
     rclcpp::shutdown();
     return 0;
